641d42b4aacce3c8bb57360d17a366f61bd96ebd
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-25 07:16:21 +0530

    Create twoSum.js

---
+++ twoSum.js
@@ -0,0 +1,11 @@
+var twoSum = function(nums, target) {
+    for(let i in nums){
+        
+        for(let j = i; j< nums.length ; j++){
+        if(nums[i]+nums[j] == target && i != j){      
+            return [i,j]         
+        }
+            
+        }
+    }
+};
