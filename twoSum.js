682051de624f1f8e139d4d4d215638eaf578db9b
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-25 07:17:05 +0530

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,11 +1,9 @@
 var twoSum = function(nums, target) {
     for(let i in nums){
-        
         for(let j = i; j< nums.length ; j++){
-        if(nums[i]+nums[j] == target && i != j){      
-            return [i,j]         
-        }
-            
+            if(nums[i]+nums[j] == target && i != j){      
+                return [i,j]         
+            }
         }
     }
 };
