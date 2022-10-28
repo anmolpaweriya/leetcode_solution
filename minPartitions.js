980546a2a93cafd6b40cad7c9cfa8038f69faa3f
Anisole <64343922+anisolepro@users.noreply.github.com>
2022-10-28 14:07:02 +0500

    1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
    A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
    
    Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.

--- minPartitions.js
+++ minPartitions.js
@@ -1,3 +1,7 @@
 var minPartitions = function(n) {
-     return n.split("").sort((a,b)=>b-a)[0]
+    let m = 0;
+    for(let i = 0; i< n.length;i++){
+        if(Number(n[i]) > m){m = Number(n[i])}
+    }
+    return m
 };
