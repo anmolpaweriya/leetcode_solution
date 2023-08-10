a8bdc19921ea4677d6107e2c3f230a985349bbcf
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-08-11 00:19:15 +0530

    2666. Allow One Function Call

---
+++ oneFunctionCall.js
@@ -0,0 +1,22 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+
+
+var once = function(fn) {
+    this.isCalled = false;
+    return function(...args){
+        if(this.isCalled )return ;
+        this.isCalled = true;
+        return fn(...args)
+    }
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
